69e940caf24c0d1525a1170b4a9c139f84c8c2a6
Hati0kooHati <dserion.prn@gmail.com>
2025-06-24 09:19:59 +0600

    Update todo_list.dart

--- todo_app 1.0/lib/widget/todo_list.dart
+++ todo_app 1.0/lib/widget/todo_list.dart
@@ -5,26 +5,19 @@ import 'package:todo_app/provider/todo_provider.dart';
 import 'package:todo_app/widget/todo_widget.dart';
 
 
-class TodoList extends StatefulWidget {
+class TodoList extends StatelessWidget {
   const TodoList({super.key, required this.todoProvider});
 
   final TodoProvider todoProvider;
 
   @override
-  State<TodoList> createState() => _TaskListState();
-}
-
-class _TaskListState extends State<TodoList> {
-
-
-  @override
   Widget build(BuildContext context) {
-    final List<Todo> todoList = widget.todoProvider.todoList;
+    final List<Todo> todoList = todoProvider.todoList;
     return Expanded(
       child: ListView.builder(
         itemCount: todoList.length,
         itemBuilder: (context, index) {
-          return TodoWidget(todo: todoList[index], todoProvider: widget.todoProvider,);
+          return TodoWidget(todo: todoList[index], todoProvider: todoProvider,);
         },
       ),
     );
